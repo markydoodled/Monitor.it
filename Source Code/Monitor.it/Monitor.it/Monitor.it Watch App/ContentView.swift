4c94e52460bdd603bfa389fd872d19b6b91c936c
Mark Howard <markhoward2005@gmail.com>
2022-11-07 22:02:56 +0000

    Added watchOS 9 Rewrite Project

---
+++ Source Code/Monitor.it/Monitor.it/Monitor.it Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Monitor.it Watch App
+//
+//  Created by Mark Howard on 07/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source Code/Monitor.it/Monitor.it/Monitor.it Watch App/Monitor_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monitor_itApp.swift
+//  Monitor.it Watch App
+//
+//  Created by Mark Howard on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Monitor_it_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
